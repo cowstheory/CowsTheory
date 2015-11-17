758a0bea3f29cd8dddff021c136815006e9593bb
Victor Hung <vhung@kth.se>
2015-11-17 19:18:28 +0100

    Added TODOs in Xbox1.cs and Xbox2.cs

--- Assets/Scripts/Controllers/Xbox1.cs
+++ Assets/Scripts/Controllers/Xbox1.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Xbox1 : Controller {
 
 	void Update () {
+		// TODO: Custom mapping for xbox controller 1 
 		Vector3 direction = new Vector3(Input.GetAxis("LeftJoystickX"), -Input.GetAxis("LeftJoystickY"), 0.0F);
 		if(Input.GetButtonDown("A") && direction != new Vector3()){
 			p.fireGun (direction);
--- Assets/Scripts/Controllers/Xbox2.cs
+++ Assets/Scripts/Controllers/Xbox2.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Xbox2 : Controller {
 
 	void Update () {
+		// TODO: Custom mapping for xbox controller 2
 		Vector3 direction = new Vector3(Input.GetAxis("LeftJoystickX"), -Input.GetAxis("LeftJoystickY"), 0.0F);
 		if(Input.GetButtonDown("A") && direction != new Vector3()){
 			p.fireGun (direction);
