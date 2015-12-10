8932c7e9db6ef2645e84bf3a3a74df9916cf0f05
C0rren <erik.e.dahlstrom@gmail.com>
2015-12-10 19:24:29 +0100

    random song

---
+++ Assets/RandomSong.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSong : MonoBehaviour {
+
+	public AudioClip[] songAudioClips;
+	public AudioSource songAudioSource;
+
+	void Start () {
+		//
+		songAudioSource = GetComponent<AudioSource> ();
+	}
+	
+	void Update () {
+		if (!songAudioSource.isPlaying) {
+			playRandomSong ();
+		}
+	
+	}
+	void playRandomSong(){
+
+		int randomNumber = Random.Range(0, songAudioClips.Length);
+		if (randomNumber == 0) {
+			songAudioSource.volume = 0.7F;
+		} 
+		else if (randomNumber == 1) {
+			songAudioSource.volume = 0.5F;
+		}
+		songAudioSource.clip = songAudioClips [randomNumber];
+		songAudioSource.Play ();		
+
+	}
+}
