1018fbaafa8cd7957b5cdd53da5548c14a0181ed
Victor Hung <vhung@kth.se>
2015-12-11 11:59:03 +0100

    Added particle effects

---
+++ Assets/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public GameObject whirl;
+	
+	// Update is called once per frame
+	void Update () {
+		whirl.transform.Rotate (0, 0, 360 * Time.deltaTime);
+	}
+}
